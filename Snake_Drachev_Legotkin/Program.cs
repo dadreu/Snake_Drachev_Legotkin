8d5a85cf2b819753cf0158a560789a8eb2124a18
dadreu <dadreu@gmail.com>
2024-11-23 01:40:09 +0500

    Созданный метод загрузки данных о результатах игроков. Драчёв

--- Snake_Drachev_Legotkin/Program.cs
+++ Snake_Drachev_Legotkin/Program.cs
@@ -205,6 +205,21 @@ namespace Snake_Drachev_Legotkin
             SW.WriteLine(json);
             SW.Close();
         }
+        public static void LoadLeaders()
+        {
+            if (File.Exists("./leaders.txt"))
+            {
+                StreamReader SR = new StreamReader("./leaders.txt");
+                string json = SR.ReadLine();
+                SR.Close();
+                if (!string.IsNullOrEmpty(json))
+                    Leaders = JsonConvert.DeserializeObject<List<Leaders>>(json);
+                else
+                    Leaders = new List<Leaders>();
+            }
+            else
+                Leaders = new List<Leaders>();
+        }
 
     }
 }
