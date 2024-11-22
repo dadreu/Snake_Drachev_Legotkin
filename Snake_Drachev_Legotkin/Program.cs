063e39cfa692d02489ef3e4c81f8dad14e1077d5
dadreu <dadreu@gmail.com>
2024-11-23 00:32:34 +0500

    Добавленные переменные сервера. Драчёв

--- Snake_Drachev_Legotkin/Program.cs
+++ Snake_Drachev_Legotkin/Program.cs
@@ -12,7 +12,9 @@ namespace Snake_Drachev_Legotkin
         public static List<Leaders> Leaders = new List<Leaders>();
         public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
         public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
+        
         private static int localPort = 5001;
+       
         public static int MaxSpeed = 15;
         static void Main(string[] args)
         {
