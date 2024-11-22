af979f63ec8dab423a9971d751189102eefaf99d
dadreu <dadreu@gmail.com>
2024-11-23 00:31:52 +0500

    Добавьте файлы проекта.

---
+++ Snake_Drachev_Legotkin/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Drachev_Legotkin
+{
+    internal class Program
+    {
+        public static List<Leaders> Leaders = new List<Leaders>();
+        public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+        public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
+        private static int localPort = 5001;
+        public static int MaxSpeed = 15;
+        static void Main(string[] args)
+        {
+        }
+    }
+}
