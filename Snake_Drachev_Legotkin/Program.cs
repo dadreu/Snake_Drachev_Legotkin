728d82c2d68c9277397b8b864b04c74f46d9eb89
dadreu <dadreu@gmail.com>
2024-11-23 01:49:15 +0500

    Запуск сервера. Драчёв

--- Snake_Drachev_Legotkin/Program.cs
+++ Snake_Drachev_Legotkin/Program.cs
@@ -26,6 +26,18 @@ namespace Snake_Drachev_Legotkin
         public static int MaxSpeed = 15;
         static void Main(string[] args)
         {
+            try
+            {
+                Thread tRec = new Thread(new ThreadStart(Receiver));
+                tRec.Start();
+                Thread tTime = new Thread(Timer);
+                tTime.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n " + ex.Message);
+            }
         }
         public static void Send()
         {
