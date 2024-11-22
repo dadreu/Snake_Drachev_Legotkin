1eff62bf2d02b0230bbc2b8249b411a2dd882a4e
dadreu <dadreu@gmail.com>
2024-11-23 00:53:57 +0500

    Созданный метод Send. Драчёв

--- Snake_Drachev_Legotkin/Program.cs
+++ Snake_Drachev_Legotkin/Program.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Common;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,5 +24,30 @@ namespace Snake_Drachev_Legotkin
         static void Main(string[] args)
         {
         }
+        public static void Send()
+        {
+            foreach (ViewModelUserSettings User in remoteIPAddress) {
+                UdpClient sender = new UdpClient();
+                IPEndPoint endPoint = new IPEndPoint (
+                    IPAddress.Parse(User.IPAddress),
+                    int.Parse(User.Port));
+                try {
+                    byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelGames.Find(x => x.IdSnake == User.IdSnake)));
+                    sender.Send(bytes, bytes.Length, endPoint);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Отправил данные пользователю: {User.IPAddress}:{User.Port}");
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n " + ex.Message);
+                               
+                }
+                finally
+                {
+                    sender.Close();
+                }
+        } }
+
     }
 }
