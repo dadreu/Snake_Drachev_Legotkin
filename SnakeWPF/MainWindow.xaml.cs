26af5508ba697c171cc43530045229c10e8d0d1d
Paolo <legotkinc2006@gmail.com>
2024-11-23 15:12:28 +0500

    Метод прослушивания ответов от сервера. Леготкин

--- SnakeWPF/MainWindow.xaml.cs
+++ SnakeWPF/MainWindow.xaml.cs
@@ -15,6 +15,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using Common;
+using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace SnakeWPF
 {
@@ -26,21 +30,79 @@ namespace SnakeWPF
 
         public static MainWindow mainWindow;
         public ViewModelUserSettings ViewModelUserSettings = new ViewModelUserSettings();
-        public ViewModelGames ViewModelGames null;
+        public ViewModelGames ViewModelGames = null;
         public static IPAddress remoteIPAddress = IPAddress.Parse("127.0.0.1");
         public static int remotePort = 5001;
         public Thread tRec;
         public UdpClient receivingUdpClient;
         public Pages.Home Home = new Pages.Home();
         public Pages.Game Game = new Pages.Game();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
         public void StartReceiver()
         {
             tRec = new Thread(new ThreadStart(Receiver));
             tRec.Start();
+        }
+
+        public void OpenPage(Page PageOpen)
+        {
+            DoubleAnimation startAnimation = new DoubleAnimation();
+            startAnimation.From = 1;
+            startAnimation.To = 0;
+            startAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            startAnimation.Completed += delegate
+            {
+                frame.Navigate(PageOpen);
+                DoubleAnimation endAnimation = new DoubleAnimation();
+                endAnimation.From = 0;
+                endAnimation.To = 1;
+                endAnimation.Duration = TimeSpan.FromSeconds(0.6);
+                frame.BeginAnimation(OpacityProperty, endAnimation);
+            };
+            frame.BeginAnimation(OpacityProperty, startAnimation);
+        }
+
+        public void Receiver()
+        {
+            receivingUdpClient = new UdpClient(int.Parse(ViewModelUserSettings.Port));
+            IPEndPoint RemoteIpEndPoint = null;
+            try
+            {
+                while (true)
+                {
+                    byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+                    string returnData = Encoding.UTF8.GetString(receiveBytes);
+                    if (ViewModelGames == null)
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            OpenPage(Game);
+                        });
+                    }
+
+                    ViewModelGames = JsonConvert.DeserializeObject<ViewModelGames>(returnData.ToString());
+                    if (ViewModelGames.SnakesPlayers.GameOver)
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            OpenPage(new Pages.EndGame());
+                        });
+                    }
+                    else
+                    {
+                        Game.CreateUI();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Возникло исключение:" + ex.ToString() + "\n " + ex.Message);
+            }
         }
     }
 }
